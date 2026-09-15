fba830eb1ff1d517580d3b84fa0af8da3f2aa88a
b-varas <b.varas.beltran@gmail.com>
2026-09-15 19:46:38 -0300

    se crean y se actualizan los modelos de producto, cliente y user

---
+++ app/Models/Producto.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Producto extends Model
+{
+    protected $fillable = [
+        'sku',
+        'nombre',
+        'descripcion corta',
+        'descripcion larga',
+        'imagen',
+        'precio_neto',
+        'precio_venta',
+        'stock_actual',
+        'stock_minimo',
+        'stock_bajo',
+        'stock_alto'
+    ];
+}
